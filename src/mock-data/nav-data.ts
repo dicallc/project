e6bb59fed0ecdf5ad3895343eeb46f799c124da8
蒋迪凯 <jiangdk@e-lead.cn>
2017-08-15 23:25:56 +0800

    第一次提交

--- src/mock-data/nav-data.ts
+++ src/mock-data/nav-data.ts
@@ -7,15 +7,15 @@ export const  NAVS: any = [
     "routerUrl": "/all",
     "navIcon": "all",
     "navText": "All",
-    "introTitle": "前端编程之路",
-    "intro": "个人业余在 Github 上开发的部分项目/DEMO，擅长于JavaScript、熟悉Node.js、掌握Java"
+    "introTitle": "小新的编程之路",
+    "intro": "我(谷歌粉，收藏癖)，个人业余在 Github 上开发的部分项目/DEMO，擅长于Android、Angular、熟悉Node.js、掌握Java"
   },
   {
-    "routerUrl": "/vue",
-    "navIcon": "fe",
-    "navText": "Vue.js",
-    "introTitle": "Vue.js 项目",
-    "intro": "vue.js相关项目、demo"
+    "routerUrl": "/app",
+    "navIcon": "android",
+    "navText": "App",
+    "introTitle": "App 项目",
+    "intro": "App 相关项目、demo。工作上开发App使用过的技术有：Android、Ionic、Weex，业余开发学习用过RN，个人博客也有分享关于APP开发的技术文章"
   },
   {
     "routerUrl": "/angular",
@@ -23,6 +23,13 @@ export const  NAVS: any = [
     "navText": "Angular",
     "introTitle": "Angular 项目",
     "intro": "Angular 相关项目、demo"
+  },
+  {
+    "routerUrl": "/vue",
+    "navIcon": "fe",
+    "navText": "Vue.js",
+    "introTitle": "Vue.js 项目",
+    "intro": "vue.js相关项目、demo"
   },
   {
     "routerUrl": "/react",
@@ -37,13 +44,6 @@ export const  NAVS: any = [
     "navText": "Node.js",
     "introTitle": "Node.js 项目",
     "intro": "Node.js 相关项目、demo、NPM插件",
-  },
-  {
-    "routerUrl": "/app",
-    "navIcon": "android",
-    "navText": "App",
-    "introTitle": "App 项目",
-    "intro": "App 相关项目、demo。工作上开发App使用过的技术有：Ionic、NativeScript，业余开发学习用过RN，个人博客也有分享关于APP开发的技术文章"
   },
   {
     "routerUrl": "/other",
