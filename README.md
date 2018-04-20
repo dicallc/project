5dae00e369652766d056b71f4cbd464a58dd159e
jiangdk <jiangdk@e-lead.cn>
2018-04-20 11:34:36 +0800

    更新项目信息

--- README.md
+++ README.md
@@ -22,13 +22,5 @@ SPA site for displaying owner project built with angular，模板仿慕课
 > npm run build:aot
 
 
-## License
 
-MIT
-
----
-
-> [giscafer.com](http://giscafer.com) &nbsp;&middot;&nbsp;
-> GitHub [@giscafer](https://github.com/giscafer) &nbsp;&middot;&nbsp;
-> Weibo [@Nickbing Lao](https://weibo.com/laohoubin)
 
--- src/mock-data/nav-data.ts
+++ src/mock-data/nav-data.ts
@@ -1,55 +1,45 @@
-/**
- * Created by giscafer on 2017/5/13.
- */
-
-export const  NAVS: any = [
+export const NAVS: any = [
   {
-    "routerUrl": "/all",
-    "navIcon": "all",
-    "navText": "All",
-    "introTitle": "小新的编程之路",
-    "intro": "我(谷歌粉，收藏癖)，个人业余在 Github 上开发的部分项目/DEMO，擅长于Android、Angular、熟悉Node.js、掌握Java"
+    'routerUrl': '/all',
+    'navIcon': 'all',
+    'navText': '全部',
+    'introTitle': '小新的编程之路',
+    'intro': '我(谷歌粉，收藏癖)，个人业余在 Github 上开发的部分项目/DEMO，擅长于Android、Angular、熟悉Node.js'
   },
   {
-    "routerUrl": "/app",
-    "navIcon": "android",
-    "navText": "App",
-    "introTitle": "App 项目",
-    "intro": "App 相关项目、demo。工作上开发App使用过的技术有：Android、Ionic、Weex，业余开发学习用过RN，个人博客也有分享关于APP开发的技术文章"
+    'routerUrl': '/app',
+    'navIcon': 'android',
+    'navText': 'App',
+    'introTitle': 'App 项目',
+    'intro': 'App 相关项目、demo。工作上开发App使用过的技术有：Android、Ionic、Weex，业余开发学习用过RN，个人博客也有分享关于APP开发的技术文章'
   },
   {
-    "routerUrl": "/angular",
-    "navIcon": "php",
-    "navText": "Angular",
-    "introTitle": "Angular 项目",
-    "intro": "Angular 相关项目、demo"
+    'routerUrl': '/h5',
+    'navIcon': 'fe',
+    'navText': 'H5混合开发',
+    'introTitle': 'H5项目',
+    'intro': '专门给手机端开发的应用，不局限平台，技术'
   },
   {
-    "routerUrl": "/vue",
-    "navIcon": "fe",
-    "navText": "Vue.js",
-    "introTitle": "Vue.js 项目",
-    "intro": "vue.js相关项目、demo"
+    'routerUrl': '/angular',
+    'navIcon': 'php',
+    'navText': 'Angular',
+    'introTitle': 'Angular 项目',
+    'intro': 'Angular 相关项目、demo'
   },
   {
-    "routerUrl": "/react",
-    "navIcon": "java",
-    "navText": "React",
-    "introTitle": "React 项目",
-    "intro": "React 相关项目、demo",
-  },
+    'routerUrl': '/nodejs',
+    'navIcon': 'ios',
+    'navText': 'Node.js',
+    'introTitle': 'Node.js 项目',
+    'intro': 'Node.js 相关项目、demo、NPM插件',
+  }
+  ,
   {
-    "routerUrl": "/nodejs",
-    "navIcon": "ios",
-    "navText": "Node.js",
-    "introTitle": "Node.js 项目",
-    "intro": "Node.js 相关项目、demo、NPM插件",
-  },
-  {
-    "routerUrl": "/other",
-    "navIcon": "fe",
-    "navText": "Other",
-    "introTitle": "其他项目",
-    "intro": "微信公众号/微信小程序开发、Java开发、其他非框架技术开发Demo"
+    'routerUrl': '/other',
+    'navIcon': 'fe',
+    'navText': 'Other',
+    'introTitle': '其他项目',
+    'intro': '微信公众号/微信小程序开发、Java开发、其他非框架技术开发Demo'
   }
 ];
