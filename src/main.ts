8841b6e18dd0466368510cb1656b112a1eea890d
619338136@qq.com <kaisa008>
2019-03-21 17:57:24 +0800

    更新类容

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-enableProdMode();//enable the production mode.
+import {environment} from './environments/environment';
+if (environment.production) {
+  enableProdMode();
+}
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+// platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch(err => console.error(err));
